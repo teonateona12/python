7c4cba47be6d6b8fef97692119cb552d8b24f47d
teonateona12 <teona.piranishvili1@gmail.com>
2023-07-31 16:24:36 +0400

    feat: create hangman game

--- hangman.py
+++ hangman.py
@@ -1,1 +1,20 @@
-# hangman
+import random 
+word_list = ['ardvark', 'baboon', 'camel', 'love', 'happy']
+chosen_word = random.choice(word_list)
+display = ['_'] * len(chosen_word)
+
+while '_' in display:
+  guess = input('Choose letter: ').lower()
+  for position in range(len(chosen_word)):
+    letter = chosen_word[position]
+    if letter == guess:
+      display[position] = letter
+  print(display)
+
+if '_' not in display:
+    print('You Win!')
+  
+
+
+
+  
